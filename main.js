4962af903ca19e52be184c4e6edb4f0647db7657
Ikerstreamer <ikerdeaguirre@hotmail.com>
2018-09-27 10:05:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var Tab = document.body;
-Tab.insertAdjacentHTML('beforeend','<form>Console: <input type="text" id="cInput"><br>Global-Scope:<input type="checkbox" id="Gscope"><br>Scope: <input type="text" id="sInput"></form><br><b id="console"></b><br>');
-setInterval(function(){
-	if(document.getElementById("Gscope").checked) document.getElementById("console").innerHTML = window[document.getElementById("cInput").value];
-    else document.getElementById("console").innerHTML = window[document.getElementById("sInput").value][document.getElementById("cInput").value];
-},50)
+Tab.insertAdjacentHTML('beforeend','<textarea id ="console1"></textarea>');
+getElementById('console1').addEventListener('change', () => {eval(getElementById('console1').value)}
+)
